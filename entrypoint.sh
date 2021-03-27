4dfc127e8fc60d8a08ed8099feddb59035b0e2e1
darkquasar <darkquasar7@gmail.com>
2021-03-27 21:41:25 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,15 @@
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
-echo "Starts"
+echo "[+] Action start"
 SOURCE_DIRECTORY="$1"
-DESTINATION_GITHUB_USERNAME="$2"
-DESTINATION_REPOSITORY_NAME="$3"
-USER_EMAIL="$4"
-DESTINATION_REPOSITORY_USERNAME="$5"
-TARGET_BRANCH="$6"
-COMMIT_MESSAGE="$7"
+DESTINATION_DIRECTORY="$2"
+DESTINATION_GITHUB_USERNAME="$3"
+DESTINATION_REPOSITORY_NAME="$4"
+USER_EMAIL="$5"
+DESTINATION_REPOSITORY_USERNAME="$6"
+TARGET_BRANCH="$7"
+COMMIT_MESSAGE="$8"
 
 if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
 then
@@ -19,24 +20,21 @@ fi
 
 CLONE_DIR=$(mktemp -d)
 
-echo "Cloning destination git repository"
+echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$DESTINATION_GITHUB_USERNAME"
 git clone --single-branch --branch "$TARGET_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
-TARGET_DIR=$(mktemp -d)
-mv "$CLONE_DIR/.git" "$TARGET_DIR"
+echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $BLa in git repo $DESTINATION_REPOSITORY_NAME"
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$DESTINATION_DIRECTORY"
+cd "$CLONE_DIR/$DESTINATION_DIRECTORY"
 
-echo "Copying contents to git repo"
-cp -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR"
-cd "$TARGET_DIR"
-
-echo "Files that will be pushed"
+echo "[+] Files that will be pushed"
 ls -la
 
-echo "Adding git commit"
+echo "[+] Adding git commit"
 
 ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
@@ -47,6 +45,6 @@ git status
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
-echo "Pushing git commit"
+echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
 git push origin --set-upstream "$TARGET_BRANCH"
